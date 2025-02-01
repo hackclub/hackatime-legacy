1f4dc2e5412cd9cb4faae76bcb989afa59cd2241
Ben <malted@malted.dev>
2025-02-01 00:18:16 -0500

    Update interval.go

--- helpers/interval.go
+++ helpers/interval.go
@@ -78,8 +78,9 @@ func ResolveIntervalTZ(interval *models.IntervalKey, tz *time.Location) (err err
 		from = time.Date(2024, 10, 3, 0, 0, 0, 0, time.Local)
 		to = time.Date(2025, 1, 12, 23, 59, 59, 0, time.Local)
 	case models.IntervalHighSeas:
+		est, _ := time.LoadLocation("America/New_York")
 		from = time.Date(2024, 10, 30, 0, 0, 0, 0, time.Local)
-		to = time.Date(2025, 1, 31, 23, 59, 59, 0, time.Local)
+		to = time.Date(2025, 1, 31, 0, 0, 0, 0, est)
 	default:
 		err = errors.New("invalid interval")
 	}
